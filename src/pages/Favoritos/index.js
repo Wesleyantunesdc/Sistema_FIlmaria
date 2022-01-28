e5b6270d2d8b773785ed70c0ca999e6ff1b902d4
@wesleyantunesdca <wesleyantunesdca@gmail.com>
2022-01-28 14:07:04 -0300

    Implementacao da pagina de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,51 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './favoritos.css'
 
-export default function Favoritos(){
+
+export default function Favoritos() {
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || [])
+    }, [])
+
+
+    function remover(id) {
+        let listaAtualizada = filmes.filter(f => {
+            if (f.id !== id)
+                return f
+        })
+        setFilmes(listaAtualizada)
+        localStorage.setItem('filmes', JSON.stringify(listaAtualizada))
+    }
+
     return (
-        <div>
+        <div className='container'>
             <h1>Filmes Favoritos</h1>
+            <div className='filmes'>
+                {filmes.map(f => {
+                    return (
+                        <div className='filme' key={f.id}>
+                            <div className='filme-item-esquerda'>
+                                <h2>
+                                    {f.nome}
+                                </h2>
+                            </div>
+                            <div className='filme-item-direita'>
+                                <button className='btn-detalhes'>
+                                    <Link to={`/filmes/${f.id}`}>
+                                        Ver Detalhes
+                                    </Link>
+                                </button>
+                                <button className='btn-excluir' onClick={() => remover(f.id)}> Remover Filme</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
